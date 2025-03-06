7bd481d4d713440cc2c57b2bfe343f3b0b4b3750
Maxtron <maxtron@Maxtrons-MacBook-Pro.local>
2025-03-06 18:01:56 +0530

    header index

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -13,8 +13,8 @@ import { motion } from "motion/react";
 const Header = () => {
   const [isOpen, setIsOpen] = useState(false);
   return (
-    <div className="xl:p-8 px-5 py-4 w-full">
-      <header className="   z-10 flex flex-row justify-between items-center  w-full rounded-[56px] border-[4px] border-solid border-border bg-text px-5 py-2  xl:px-8 xl:py-3 ">
+    <div className="xl:p-8 px-5 py-2 w-full">
+      <header className="z-10 flex flex-row justify-between items-center  w-full rounded-[56px] border-[4px] border-solid border-border bg-text px-5 py-2  xl:px-8 xl:py-3 ">
         <div className="flex flex-row items-center justify-start  gap-0">
           <img
             src={"/images/logo.png"}
